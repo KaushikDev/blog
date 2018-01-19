af7a33df0c42f2e3c87d53e6c5923f88dac98c9d
Piyush Kaushik <piyush.kaushik02@gmail.com>
2018-01-19 12:07:13 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -37,12 +37,11 @@ var replyId;
 				console.log("retrieved comment is : "+retrievedData.Comment);
 				console.log("retrieved name is : "+retrievedData.Name);	
 				console.log("retrieved key is : "+retrievedKey);
-				if(snap.child("Replies")){
+				if(snap.child("Replies").exists()){
 					var retrievedChildData = snap.child("Replies").val();
 					console.log("retrived replier is :"+retrievedChildData.Replier);
-				console.log()
 				}	
-				console.log("retrieved replier name is : "+snap.child("Replies").Replier);
+				console.log("retrieved replier name is : "+snap.child().Replier);
 				$("#commentList").append("<div><label style='width:100%;'>"+retrievedData.Name+" says.."+"</label><p style='width:100%;background-color:#808080;font-style:italic;'>"+retrievedData.Comment+"</p><button id="+"'"+retrievedKey+"'"+" style='background-color:red;border-radius:5px' data-toggle='modal' data-target='#replyModal' class='btn btn-sm' onClick='storeReply(this.id)'>"+"Reply"+"</button></div>");
 				i++;
 					});
