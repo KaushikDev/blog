397976c9628d5888caecd053e271be368475d166
kaushikDev <piyush.kaushik02@gmail.com>
2019-02-11 23:55:14 +0530

    final build

--- logic.js
+++ logic.js
@@ -81,7 +81,7 @@ var numChilds;
 					$("#commentList").append("<div class='card row'><div class='col-xs-12'><label style='width:100%;'>["+retrievedCommentData.TimestampC+"] "+retrievedCommentData.Name+" said.."+"</label><p style='width:100%;font-style:italic;'>\""+retrievedCommentData.Comment+"\"</p></div>"+htmlStr+"<button id="+"'"+retrievedCommentKey+"'"+" class='btn btn-success' data-toggle='modal' data-target='#replyModal' class='btn' onClick='storeReply(this.id)'>"+"Reply"+"</button></div>");	
 					}
 				else {
-				$("#commentList").append("<div class='card row'><div class='col-xs-12'><label style='width:100%;'>["+retrievedCommentData.TimestampC+"]  "+retrievedCommentData.Name+" said.."+"</label><p style='width:100%;font-style:italic;'>\""+retrievedCommentData.Comment+"\"</p></div><button id="+"'"+retrievedCommentKey+"'"+" class='btn btn-success data-toggle='modal' data-target='#replyModal' class='btn btn-sm' onClick='storeReply(this.id)'>"+"Reply"+"</button></div>");	
+				$("#commentList").append("<div class='card row'><div class='col-xs-12'><label style='width:100%;'>["+retrievedCommentData.TimestampC+"]  "+retrievedCommentData.Name+" said.."+"</label><p style='width:100%;font-style:italic;'>\""+retrievedCommentData.Comment+"\"</p></div><button id="+"'"+retrievedCommentKey+"'"+" class='btn btn-success' data-toggle='modal' data-target='#replyModal' class='btn' onClick='storeReply(this.id)'>"+"Reply"+"</button></div>");	
 				}	
 				
 								
